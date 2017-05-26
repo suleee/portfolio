7e1170f402ca64b55f465fac2e8eb7e2e4c8f667
Jisu <jaejisu@gmail.com>
2017-05-26 09:24:28 -0700

    add links in the web page

--- web.php
+++ web.php
@@ -2,7 +2,7 @@
 <?php include( './pages/header.php' );?>
 <!-- side menu -->
 <?php include( './pages/side.php' );?>
-	
+
 <div class="container">
 
 		<div class="content-header">
@@ -15,12 +15,16 @@
 		<h3>UI/UX Design</h3>
 		<p>Create variable websites based on the mobile-first: responsive design. Ensure proper communication with producer and clients about the concepts and rationale of the website before starting a project. Then iterating on design and show the progression from concept to final design. This section will show the wireframe of the website and style guide that I have done previously. </p>
 		<img class="web-img" src="img/web-img01.png" alt="img01">
-
+		<ul class="viewmore">
+			<a href="http://jaejisu.wixsite.com/jisulee/blank-cjry"><li>View fishing hero UI</li></a>
+			<a href="http://jaejisu.wixsite.com/jisulee/blank-ct9w"><li>View Duple wrap website UI</li></a>
+			<a href="http://jaejisu.wixsite.com/jisulee/blank-c15h7"><li>View TTE website UI</li></a>
+		</ul>
 	</div>
 
 	<div class="page-section2">
 	<h3>Wireframe & Style Guide</h3>
-	
+
 	<img class="web-img02" src="img/web-img02.png" alt="img02">
 	</div>
 	</div>
